8d1cb7f9ec6b9663ddcad31e109e447087f64ca3
Pedro Chitarra <pedrochitarra@hotmail.com>
2021-06-22 21:54:28 -0300

    ADD: Lesson 09 from Intro To Vue2 from Begginer Path: Communicating Events

---
+++ begineer-path/intro-to-vue2/09 communicating-events/vue-instance.js
@@ -0,0 +1,142 @@
+//Create a new component for product-details with a prop of details. 
+
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+})
+
+
+
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    },
+    cart: {
+      type: Array
+    }
+  },
+  template: `
+   <div class="product">
+        
+      <div class="product-image">
+        <img :src="image" />
+      </div>
+
+      <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <product-details :details="details"></product-details>
+
+          <div class="color-box"
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+          </div> 
+
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
+
+          <button v-on:click="removeFromCart" 
+            :disabled="cart.length == 0"
+            :class="{ disabledButton: cart.length == 0 }"
+          >
+          Remove from cart
+          </button>
+
+
+
+       </div>  
+    
+    </div>
+   `,
+  data() {
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage:  'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            variantQuantity: 10     
+          },
+          {
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0     
+          }
+        ],
+
+    }
+  },
+    methods: {
+      addToCart: function() {
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+      },
+      removeFromCart: function () {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+      },
+      updateProduct: function(index) {  
+          this.selectedVariant = index
+      }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product  
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+          if (this.premium) {
+            return "Free"
+          }
+            return 2.99
+        }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      premium: true,
+      cart: []
+    },
+    methods: {
+      addToCart (id) {
+        this.cart.push(id)
+      },     
+      removeFromCart (id) {
+        const indexToRemove = this.cart.indexOf(id)
+        console.log(indexToRemove)
+        this.cart.splice(indexToRemove, 1)
+      },
+      
+    }
+})
