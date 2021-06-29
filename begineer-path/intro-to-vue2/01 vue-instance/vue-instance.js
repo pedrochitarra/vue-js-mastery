618cdb03d1038408772c9c1ceed3795ee0a76e87
Pedro Chitarra <pedrochitarra@hotmail.com>
2021-06-28 21:43:52 -0300

    MODIFY: Folder reorganization

--- begineer-path/intro-to-vue2/01 vue-instance/vue-instance.js
+++
@@ -1,7 +0,0 @@
-let app = new Vue ({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        description: 'A pair of warm, fuzzy socks'
-    }
-})
--- begineer-path/intro-to-vue2/02 attribute-binding/vue-instance.js
+++
@@ -1,9 +0,0 @@
-let app = new Vue ({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        description: 'A pair of warm, fuzzy socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
-        link: 'http://google.com'
-    }
-})
